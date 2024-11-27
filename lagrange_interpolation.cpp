22e9ba2de2afa89f53c4c2ea7b61f190beff64ba
Saim Ahmed Emon <81570237+EmonThePrince@users.noreply.github.com>
2024-11-27 15:41:10 +0600

    Update lagrange_interpolation.cpp

--- lagrange_interpolation.cpp
+++ lagrange_interpolation.cpp
@@ -12,16 +12,31 @@ public:
         y = yy;
     }
 
-    double interpolate(double xi) {
+    double runX(double xi) {
         double res = 0;
         int n = x.size();
         for (int i = 0; i < n; i++) {
-            double term = y[i];
+            double c = y[i];
             for (int j = 0; j < n; j++) {
                 if (i != j)
-                    term *= (xi - x[j]) / (x[i] - x[j]);
+                    c *= (xi - x[j]) / (x[i] - x[j]);
             }
-            res += term;
+            res += c;
+        }
+        return res;
+    }
+    double runY(double yi) {
+        double res = 0;
+        int n = y.size();
+        for (int i = 0; i < n; i++) {
+            double c = x[i];
+            for (int j = 0; j < n; j++) {
+                if (i != j){
+                    c *= (yi - y[j]) / (y[i] - y[j]);
+                    
+                } 
+            }
+            res += c;
         }
         return res;
     }
@@ -30,7 +45,11 @@ public:
 int main() {
     vector<double> x = {2, 2.5, 3.0};
     vector<double> y = {0.69315, 0.91629, 1.09861};
+    vector<double> x1 = {1, 3, 4};
+    vector<double> y1 = {4, 12, 19};
     Lagrange lag(x, y);
-    cout << lag.interpolate(2.7) <<endl;
+    Lagrange lag1(x1,y1);
+    cout << lag.runX(2.7) <<endl;
+    cout << lag1.runY(7) <<endl;
     return 0;
 }
